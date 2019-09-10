6a189f2e76f7c45d5f1dd2f00b334caac1358e4d
Maxim Gubin <maxim.gubin@m-kopa.com>
2019-09-10 21:57:47 +0300

    Implement infrastructure necessary for M2M load testing
    Related work items: #68179

---
+++ Netling.Core.BulkHttpClientWorker/BulkHttpClientWorkerJob.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Netling.Core.Models;
+
+namespace Netling.Core.BulkHttpClientWorker
+{
+    public class BulkHttpClientWorkerJob : IWorkerJob
+    {
+        private readonly int _index;
+        private readonly Uri _uri;
+        private readonly Stopwatch _stopwatch;
+        private readonly Stopwatch _localStopwatch;
+        private readonly WorkerThreadResult _workerThreadResult;
+        private readonly IEnumerable<IPAddress> _ipRange;
+        private readonly HttpClient _httpClient;
+        private readonly StringContent _httpContentPayload;
+        private readonly ConcurrentBag<(IPAddress, Uri)> _iPAddresses;
+
+        // Used to approximately calculate bandwidth
+        private static readonly int MissingHeaderLength = "HTTP/1.1 200 OK\r\nContent-Length: 123\r\nContent-Type: text/plain\r\n\r\n".Length; 
+
+        public BulkHttpClientWorkerJob(Uri uri)
+        {
+            _uri = uri;
+        }
+
+        private BulkHttpClientWorkerJob(
+            int index, 
+            Uri uri, 
+            WorkerThreadResult workerThreadResult, 
+            IEnumerable<IPAddress> ipRange)
+        {
+            _index = index;
+            _uri = uri;
+            _stopwatch = Stopwatch.StartNew();
+            _localStopwatch = new Stopwatch();
+            _workerThreadResult = workerThreadResult;
+            _ipRange = ipRange;
+            _httpClient = new HttpClient();
+
+            _iPAddresses = new ConcurrentBag<(IPAddress, Uri)>(ipRange.Select(ip => (ip, new Uri(uri, $"devices/{ip}/12500/test"))));
+            _httpContentPayload = new StringContent("2102F40C23060A00C35A9F22240F33353738303330343836363635333731011632048E001D4F30048002040022060A0099579F22360C100E06012204D3053447AE01");
+        }
+
+        public async ValueTask DoWork()
+        {
+            _localStopwatch.Restart();
+
+            using (var response = await _httpClient.PostAsync(_uri, _httpContentPayload))
+            {
+                var contentStream = await response.Content.ReadAsStreamAsync();
+                var length = contentStream.Length + response.Headers.ToString().Length + MissingHeaderLength;
+                var responseTime = (float)_localStopwatch.ElapsedTicks / Stopwatch.Frequency * 1000;
+                var statusCode = (int)response.StatusCode;
+
+                if (statusCode < 400)
+                {
+                    _workerThreadResult.Add((int)_stopwatch.ElapsedMilliseconds / 1000, length, responseTime, statusCode, _index < 10);
+                }
+                else
+                {
+                    _workerThreadResult.AddError((int)_stopwatch.ElapsedMilliseconds / 1000, responseTime, statusCode, _index < 10);
+                }
+            }
+        }
+
+        public WorkerThreadResult GetResults()
+        {
+            return _workerThreadResult;
+        }
+
+        public ValueTask<IWorkerJob> Init(int index, WorkerThreadResult workerThreadResult)
+        {
+            return new ValueTask<IWorkerJob>(new BulkHttpClientWorkerJob(index, _uri, workerThreadResult, _ipRange));
+        }
+    }
+}
---
+++ Netling.M2M.ConsoleClient/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CommandLine.Options;
+using Netling.Core;
+using Netling.Core.BulkHttpClientWorker;
+using Netling.Core.Models;
+using Netling.Core.SocketWorker;
+
+namespace Netling.M2M.ConsoleClient
+{
+    class Program
+    {
+        const string Uri = @"http://localhost:36004/";
+
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en");
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en");
+
+            RunWithDuration(new Uri(Uri), 8, TimeSpan.FromSeconds(60)).Wait();
+        }
+
+        private static void ShowHelp()
+        {
+            Console.WriteLine(HelpString);
+        }
+
+        private static Task RunWithDuration(Uri uri, int threads, TimeSpan duration)
+        {
+            Console.WriteLine(StartRunWithDurationString, duration.TotalSeconds, uri, threads);
+            return Run(uri, threads, duration);
+        }
+
+        private static async Task Run(Uri uri, int threads, TimeSpan duration)
+        {
+            WorkerResult result;
+            var worker = new Worker(new BulkHttpClientWorkerJob(uri));
+
+            result = await worker.Run(uri.ToString(), threads, duration, new CancellationToken());
+
+            Console.WriteLine(ResultString,
+                result.Count,
+                result.Elapsed.TotalSeconds,
+                result.RequestsPerSecond,
+                result.Bandwidth,
+                result.Errors,
+                result.Median,
+                result.StdDev,
+                result.Min,
+                result.Max,
+                GetAsciiHistogram(result));
+        }
+
+        private static string GetAsciiHistogram(WorkerResult workerResult)
+        {
+            if (workerResult.Histogram.Length == 0)
+            {
+                return string.Empty;
+            }
+
+            const string filled = "█";
+            const string empty = " ";
+            var histogramText = new string[7];
+            var max = workerResult.Histogram.Max();
+
+            foreach (var t in workerResult.Histogram)
+            {
+                for (var j = 0; j < histogramText.Length; j++)
+                {
+                    histogramText[j] += t > max / histogramText.Length * (histogramText.Length - j - 1) ? filled : empty;
+                }
+            }
+
+            var text = string.Join("\r\n", histogramText);
+            var minText = string.Format("{0:0.000} ms ", workerResult.Min);
+            var maxText = string.Format(" {0:0.000} ms", workerResult.Max);
+            text += "\r\n" + minText + new string('=', workerResult.Histogram.Length - minText.Length - maxText.Length) + maxText;
+            return text;
+        }
+
+        private const string HelpString = @"
+Usage: netling [-t threads] [-d duration] url
+
+Options:
+    -t count        Number of threads to spawn.
+    -d count        Duration of the run in seconds.
+    -c count        Amount of requests to send on a single thread.
+
+Examples:
+    netling http://localhost:5000/
+    netling http://localhost:5000/ -t 8 -d 60
+    netling http://localhost:5000/ -c 3000
+";
+
+        private const string StartRunWithCountString = @"
+Running {0} test @ {1}";
+
+        private const string StartRunWithDurationString = @"
+Running {0}s test with {2} threads @ {1}";
+
+        private const string ResultString = @"
+{0} requests in {1:0.##}s
+    Requests/sec:   {2:0}
+    Bandwidth:      {3:0} mbit
+    Errors:         {4:0}
+Latency
+    Median:         {5:0.000} ms
+    StdDev:         {6:0.000} ms
+    Min:            {7:0.000} ms
+    Max:            {8:0.000} ms
+
+{9}
+";
+    }
+}
